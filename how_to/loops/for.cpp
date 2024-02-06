98286c50628b613fa64070e3692cbc549e568ffd
ilvmikan <mattdias30@gmail.com>
2024-02-06 08:02:44 -0300

    adicionando outro exemplo do loop for

--- how_to/loops/for.cpp
+++ how_to/loops/for.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-
-    for (int x = 0; x <= 10; x++){
-        cout << "O valor de x = " << x << endl;
-    }
-
 /* Como utilizar
 
     for ( inicialização ; condição ; incremento) {
         // bloco de código
     }
-
 */
+
+void for_loop(){
+    for (int x = 0; x <= 10; x++){
+        cout << "O valor de x = " << x << endl;
+    }
+}
+
+int main(){
+    char saudacao[4] = {'O', 'i', '!', '\0'};
+
+    for(int i = 0; i < 4; i++){
+        std::cout << saudacao[i];
+    }
+
+    return 0;
 }
